7ad89838eb7027355445449b5064c25f1fd59507
Andrei Radu <andrei1.radu93@gmail.com>
2023-12-19 12:25:48 +0200

    .

--- components/fonduri/FonduriComponent.tsx
+++ components/fonduri/FonduriComponent.tsx
@@ -12,7 +12,7 @@ type Props = {
 export default function FonduriComponent({ program, index=0, carousel=false }: Props) {
     return(
         <div 
-            className={"rounded-4xl relative w-full xl:min-h-[400px] flex flex-col-reverse justify-between px-4 lg:px-20 py-2 bg-cover bg-no-repeat align-center " + (carousel ? " mb-9 lg:mb-24 xl:mb-20 mx-2 md:mx-8 lg:flex-row" : ( index == 0 ? 'mt-[2rem] md:mt-[4rem]' : 'mt-[8rem]') + ( index % 2 == 0 ? " lg:flex-row" : " lg:flex-row-reverse")) }
+            className={"rounded-4xl relative w-full xl:min-h-[400px] flex flex-col-reverse justify-between px-4 lg:px-20 py-2 bg-cover bg-no-repeat align-center " + (carousel ? " mb-9 lg:mb-24 xl:mb-24 mx-2 md:mx-8 lg:flex-row" : ( index == 0 ? 'mt-[2rem] md:mt-[4rem]' : 'mt-[8rem]') + ( index % 2 == 0 ? " lg:flex-row" : " lg:flex-row-reverse")) }
             key={program.id}
         >   
             <Image 
@@ -23,7 +23,7 @@ export default function FonduriComponent({ program, index=0, carousel=false }: P
                 className="w-full h-[360px] md:h-full absolute rounded-[35px] left-0 z-[1] top-0 object-cover object-left"
             />
             <div style={{background: "rgba(0, 0, 0, 0.45)", }} className="w-full h-[360px] md:h-full absolute rounded-[35px] left-0 z-[1] top-0"></div>
-            <div className="flex flex-col justify-center w-fit self-center items-center rounded-2xl relative top-[40px] lg:top-[100px] z-[5] bg-[#260056] py-8 px-8">
+            <div className="flex flex-col justify-center w-fit lg:max-w-[45%] self-center items-center rounded-2xl relative top-[40px] lg:top-[100px] z-[5] bg-[#260056] py-8 px-8">
                 <h5 className="text-white text-xl xl:text-4xl font-bold mb-6">{program.title}</h5>
                 <ul className="list-disc list-inside">
                     { program.bulletPoints.map((bulletPoint, index) => (
