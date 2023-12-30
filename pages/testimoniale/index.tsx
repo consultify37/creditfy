908138b0562ddda4cb5807457e459bcfc735cfe7
Natanael Moga <natanaelmoga@Natanaels-MacBook-Pro.local>
2023-12-30 15:25:10 +0200

    .

--- pages/testimoniale/index.tsx
+++ pages/testimoniale/index.tsx
@@ -47,7 +47,7 @@ export default function Testimoniale() {
   }, [scrollAmount])
   
   const [email, setEmail] = useState('')
-  
+  // .
   const upload = async (e: any) => {
     e.preventDefault()
     
