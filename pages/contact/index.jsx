85a60727aa826260503790ef376b81dc907fa387
Andrei Radu <andrei1.radu93@gmail.com>
2024-01-17 20:33:52 +0200

    .

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -285,7 +285,7 @@ export default function Contact() {
                     </div>
                     <div className="flex flex-col md:flex-row justify-center w-full items-center">
                         <ReCAPTCHA
-                            sitekey="6Ld6S_AoAAAAAMxxH9HEfvCkmAXP8lAN0Va2TRdg"
+                            sitekey="6LdWV_AoAAAAAMMdYLnmy_NUtbetbPGYWHOOhery"
                             onChange={(e) => setCaptchaVerified(!captchaVerified)}
                         />
                         { isLoading ? 
