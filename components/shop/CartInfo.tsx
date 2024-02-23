dc13212ba0b8c403eadaced26f30c932d334cefd
Andrei Radu <andrei1.radu93@gmail.com>
2024-02-23 16:30:50 +0200

    .

--- components/shop/CartInfo.tsx
+++ components/shop/CartInfo.tsx
@@ -24,25 +24,51 @@ const CartInfo = () => {
       <h1 className='text-[20px] lg:text-[35px] font-bold text-onSecondary '>La inspirely te bucuri de</h1>
 
       <div className='flex flex-col lg:flex-row justify-between gap-16 mt-16 max-w-[960px] w-full'>
-        { data.map((item, index) => {
-          const rotation = getRotationDeg(index)
-          return (
-            <div key={item.title} className='flex flex-col items-center'>
-              <div className='relative flex items-center justify-center h-[100px] w-[100px]'>
-                <Image
-                  src={item.svg}
-                  width={128}
-                  height={128}
-                  alt='.'
-                  className='w-[39px] h-auto z-10'
-                />
+        <div className='flex flex-col items-center'>
+          <div className='relative flex items-center justify-center h-[100px] w-[100px]'>
+            <Image
+              src={data[0].svg}
+              width={128}
+              height={128}
+              alt='.'
+              className='w-[39px] h-auto z-10'
+            />
 
-                <div className={`absolute h-full w-full inset-0 rounded-[20px] ${rotation}`} style={{background: getColorForWhyChoose(index != 1 ? index : 3)[0]}}></div>
-              </div>
+            <div className={`absolute h-full w-full inset-0 rounded-[20px] rotate-[20deg]`} style={{background: getColorForWhyChoose(0)[0]}}></div>
+          </div>
 
-              <p className='text-[16px] lg:text-[20px] font-semibold text-onSecondary mt-8 text-center'>{item.title}</p>
-            </div>
-          )})}
+          <p className='text-[16px] lg:text-[20px] font-semibold text-onSecondary mt-8 text-center'>{data[0].title}</p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <div className='relative flex items-center justify-center h-[100px] w-[100px]'>
+            <Image
+              src={data[1].svg}
+              width={128}
+              height={128}
+              alt='.'
+              className='w-[39px] h-auto z-10'
+            />
+
+            <div className={`absolute h-full w-full inset-0 rounded-[20px] rotate-[33deg]`} style={{background: getColorForWhyChoose(3)[0]}}></div>
+          </div>
+
+          <p className='text-[16px] lg:text-[20px] font-semibold text-onSecondary mt-8 text-center'>{data[1].title}</p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <div className='relative flex items-center justify-center h-[100px] w-[100px]'>
+            <Image
+              src={data[2].svg}
+              width={128}
+              height={128}
+              alt='.'
+              className='w-[39px] h-auto z-10'
+            />
+
+            <div className={`absolute h-full w-full inset-0 rounded-[20px] rotate-[-10deg]`} style={{background: getColorForWhyChoose(2)[0]}}></div>
+          </div>
+
+          <p className='text-[16px] lg:text-[20px] font-semibold text-onSecondary mt-8 text-center'>{data[2].title}</p>
+        </div>
       </div>
 
       <Image 
